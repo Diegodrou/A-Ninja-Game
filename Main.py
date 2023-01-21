a77993dcfcb74f5878d8b7c40719978a15577afe
Diego <diegodroulers@gmail.com>
2023-01-21 14:27:34 +0100

    added coyote time

--- Main.py
+++ Main.py
@@ -1,54 +1,58 @@
-import pygame , sys , time
+import pygame
+import sys
+import time
 from pygame.locals import *
 from levelloader import World
-from settings import*
+from settings import *
 from player import Player
 from buttons import *
 from Enemy import Enemy
 
-#Initialize Pygame and create window
+# Initialize Pygame and create window
 pygame.init()
 pygame.display.set_caption('A ninja game')
 
-#Creation of instances
+# Creation of instances
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 display = pygame.Surface(DISPLAY_SIZE)
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 18)
-font2 =pygame.font.SysFont('Arial', 60)
-#Creation of button instances for menu
+font2 = pygame.font.SysFont('Arial', 60)
+# Creation of button instances for menu
 resume_b = Button(10, 10, resume_b_img, 0.50)
-options_b = Button(10, 60, options_b_img,0.50)
+options_b = Button(10, 60, options_b_img, 0.50)
 quit_b = Button(10, 110, quit_b_img, 0.50)
 level_1_b = Button(250, 10, lvl_1_b_img, 0.50)
 back_b = Button(10, 360, back_b_img, 0.50)
 
-#Creation of button instances for paused menu
+# Creation of button instances for paused menu
 quit_b_p = Button(270, 120, quit_b_img, 0.50)
-#Creation of button instances for dead menu
-resume_b_d = Button(150,200,resume_b_img, 0.50)
+# Creation of button instances for dead menu
+resume_b_d = Button(150, 200, resume_b_img, 0.50)
 quit_b_d = Button(200, 240, quit_b_img, 0.50)
 
-#functions for the game
+# functions for the game
+
+
 def level_selection_menu():
-    #Menu variables
+    # Menu variables
     lvl_selector = True
     anim_index = 0
     BACKGROUND_ANIM = menu_bimages
     MAIN_MENU_ANIM_COOLDOWN = 150
-    update_time_m =pygame.time.get_ticks()
-    #Menu_Loop
+    update_time_m = pygame.time.get_ticks()
+    # Menu_Loop
     while lvl_selector:
-        
-        #Pygame input
+
+        # Pygame input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        #All Draws, and button instances
-        display.blit(pygame.transform.scale(BACKGROUND_ANIM[anim_index],DISPLAY_SIZE),(0,0))
-        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
+        # All Draws, and button instances
+        display.blit(pygame.transform.scale(BACKGROUND_ANIM[anim_index], DISPLAY_SIZE), (0, 0))
+        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
         if level_1_b.draw(window):
             lvl_selector = False
             Game(lvls[0])
@@ -60,30 +64,31 @@ def level_selection_menu():
         pygame.display.update()
         if pygame.time.get_ticks() - update_time_m > MAIN_MENU_ANIM_COOLDOWN:
             update_time_m = pygame.time.get_ticks()
-            if anim_index >= len(BACKGROUND_ANIM)-1 :#24 frames in the animation
+            if anim_index >= len(BACKGROUND_ANIM)-1:  # 24 frames in the animation
                 anim_index = 0
             else:
                 anim_index += 1
 
+
 def main_menu():
-    #Menu variables
+    # Menu variables
     game_menu = True
     anim_index = 0
     BACKGROUND_ANIM = menu_bimages
     MAIN_MENU_ANIM_COOLDOWN = 150
-    update_time_m =pygame.time.get_ticks()
-    #Menu_Loop
+    update_time_m = pygame.time.get_ticks()
+    # Menu_Loop
     while game_menu:
-        
-        #Pygame input
+
+        # Pygame input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        #All Draws, and button instances
-        display.blit(pygame.transform.scale(BACKGROUND_ANIM[anim_index],DISPLAY_SIZE),(0,0))
-        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
+        # All Draws, and button instances
+        display.blit(pygame.transform.scale(BACKGROUND_ANIM[anim_index], DISPLAY_SIZE), (0, 0))
+        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
         if resume_b.draw(window):
             level_selection_menu()
         if options_b.draw(window):
@@ -94,69 +99,80 @@ def main_menu():
         pygame.display.update()
         if pygame.time.get_ticks() - update_time_m > MAIN_MENU_ANIM_COOLDOWN:
             update_time_m = pygame.time.get_ticks()
-            if anim_index >= len(BACKGROUND_ANIM)-1 :#24 frames in the animation
+            if anim_index >= len(BACKGROUND_ANIM)-1:  # 24 frames in the animation
                 anim_index = 0
             else:
                 anim_index += 1
 
+
 def Game(lvl):
-    #Game Variables
+    # Game Variables & Objetcs
     game_pause = False
     fps_toggle = False
-    game_run = True 
-    DEAD_MESSAGE ='Sry Bruh U Dead'
-    DEAD_MESSAGE_TEXT = font2.render(DEAD_MESSAGE,1,pygame.Color('Red'))
-    #game instances
-    world=World(lvl)
-    player=Player(world.spawnpoint(lvl))
+    game_run = True
+    DEAD_MESSAGE = 'Sry Bruh U Dead'
+    DEAD_MESSAGE_TEXT = font2.render(DEAD_MESSAGE, 1, pygame.Color('Red'))
+    # game instances
+    world = World(lvl)
+    player = Player(world.spawnpoint(lvl))
     enemy = Enemy(world.enemy_spawn(lvl))
     player_speed = player.speed
 
-    #Sprite group creation
+    # Sprite group creation
     all_sprites = pygame.sprite.Group()
     all_sprites.add(player)
     enemy_sprites = pygame.sprite.Group()
     enemy_sprites.add(enemy)
     bullet_group = pygame.sprite.Group()
 
-    #Game functions
-    def debug_stats(color):#Shows stats useful for debugging
-        #Avg fps
+    # Game functions
+    def debug_stats(color):  # Shows stats useful for debugging
+        # Avg fps
         fps = str(int(clock.get_fps()))
-        fps_text = font.render(fps,1,pygame.Color(color))
-        #player.rect.x(left corner up)
+        fps_text = font.render(fps, 1, pygame.Color(color))
+        
+        # player.rect.x(left corner up)
         player_pos_x = 'x =' + str(player.rect.x)
-        player_pos_x_text = font.render(player_pos_x,1,pygame.Color(color))
-        #player.rect.y(left corner up)
+        player_pos_x_text = font.render(player_pos_x, 1, pygame.Color(color))
+        
+        # player.rect.y(left corner up)
         player_pos_y = 'y =' + str(player.rect.y)
-        player_pos_y_text = font.render(player_pos_y,1,pygame.Color(color))
-        #player's direction.y (vertical direction)
-        player_y_direction ='vector_y =' + str(player.direction.y)
-        player_y_direction_text = font.render(player_y_direction,1,pygame.Color(color))
-        #player's direction.x(horizontal direction)
-        player_x_direction = 'vector_x ='+ str(player.direction.x * player.speed)
-        player_x_direction_text = font.render(player_x_direction,1,pygame.Color(color))
-        #pos of the center of the player
-        player_center = 'center ='+ str(player.rect.center)
-        player_center_x_text = font.render(player_center,1,pygame.Color(color))
+        player_pos_y_text = font.render(player_pos_y, 1, pygame.Color(color))
+        
+        # player's direction.y (vertical direction)
+        player_y_direction = 'vector_y =' + str(player.direction.y)
+        player_y_direction_text = font.render(player_y_direction, 1, pygame.Color(color))
+        
+        # player's direction.x(horizontal direction)
+        player_x_direction = 'vector_x =' + str(player.direction.x * player.speed)
+        player_x_direction_text = font.render(player_x_direction, 1, pygame.Color(color))
+        
+        # pos of the center of the player
+        player_center = 'center =' + str(player.rect.center)
+        player_center_x_text = font.render(player_center, 1, pygame.Color(color))
 
-        #air timer values
+        # air timer values
         player_air_timer = "air_timer = " + str(player.air_timer)
-        player_air_timer_text = font.render(player_air_timer,1,pygame.Color(color))
+        player_air_timer_text = font.render(player_air_timer, 1, pygame.Color(color))
 
-        window.blit(fps_text,(10,0))
-        window.blit(player_pos_x_text,(10,20))
-        window.blit(player_pos_y_text,(70,20))
-        window.blit(player_x_direction_text,(10,40))
-        window.blit(player_y_direction_text,(10,60))
-        window.blit(player_center_x_text,(10,80))
-        window.blit(player_air_timer_text,(10,100))
+        # on_ground value
+        on_ground = "on_ground = " + str(player.on_ground)
+        on_ground_text = font.render(on_ground, 1, pygame.Color(color))
 
-    def update_bullet_pos(tiles,player_rect, scroll):
+        window.blit(fps_text, (10, 0))
+        window.blit(player_pos_x_text, (10, 20))
+        window.blit(player_pos_y_text, (70, 20))
+        window.blit(player_x_direction_text, (10, 40))
+        window.blit(player_y_direction_text, (10, 60))
+        window.blit(player_center_x_text, (10, 80))
+        window.blit(player_air_timer_text, (10, 100))
+        window.blit(on_ground_text, (10, 120))
+
+    def update_bullet_pos(tiles, player_rect, scroll):
         for item in bullet_group:
             item.update(tiles, player_rect, scroll)
 
-    #Game Loop
+    # Game Loop
     while game_run:
         clock.tick(FPS)
 
@@ -164,12 +180,12 @@ def Game(lvl):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_BACKSLASH:
                     fps_toggle = not fps_toggle
                     player.t_rect = not player.t_rect
-                
+
                 if event.key == pygame.K_ESCAPE:
                     if game_pause:
                         game_pause = False
@@ -183,8 +199,9 @@ def Game(lvl):
                     player.moving_right = True
                 if event.key == pygame.K_LEFT:
                     player.moving_left = True
-                if event.key in [K_SPACE,K_UP] :
-                    if player.air_timer < 3 :
+                if event.key in [K_SPACE, K_UP]:
+                    if player.air_timer < 7:
+                        # player.is_jumping = True
                         player.jump()
 
             if event.type == pygame.KEYUP:
@@ -192,24 +209,27 @@ def Game(lvl):
                     player.moving_right = False
                 if event.key == pygame.K_LEFT:
                     player.moving_left = False
+                #if event.key in [K_SPACE, K_UP]:
+                #    player.air_timer = 0
 
-        #all_sprite updates
+        # all_sprite updates
         tile_rects = world.run()
         screen_scroll = player.movementANDcollisions(tile_rects, enemy.rect)
         player.update()
-        enemy.AI(player.rect,player.dead,tile_rects,screen_scroll)
+        enemy.AI(player.rect, player.dead, tile_rects, screen_scroll)
         if enemy.Shoot:
             Bullet = enemy.shoot()
             bullet_group.add(Bullet)
         update_bullet_pos(tile_rects, player.rect, screen_scroll)
         enemy.update(player)
-        
+
         # - all draws -
 
-        display.blit(pygame.transform.scale(city_background,DISPLAY_SIZE),(0,0))
-        world.draw(display,screen_scroll)
+        display.blit(pygame.transform.scale(
+            city_background, DISPLAY_SIZE), (0, 0))
+        world.draw(display, screen_scroll)
 
-        #for item in all_sprites:
+        # for item in all_sprites:
         #     item.draw(display)
         player.draw(display)
 
@@ -219,30 +239,32 @@ def Game(lvl):
         for item in enemy_sprites:
             item.draw(display)
 
-        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
-        
+        window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
+
         if fps_toggle:
             debug_stats("Red")
 
-        #check if game is paused
-        if game_pause :
-            pygame.draw.rect(window, (255,120,219),pygame.Rect(150, 30, 300 , 300), 0,3)
-            #paused menu buttons
+        # check if game is paused
+        if game_pause:
+            pygame.draw.rect(window, (255, 120, 219),
+                             pygame.Rect(150, 30, 300, 300), 0, 3)
+            # paused menu buttons
 
-            if quit_b_p.draw(window) :
+            if quit_b_p.draw(window):
                 main_menu()
         if player.dead:
-            pygame.draw.rect(window,(255, 120, 219), pygame.Rect(0, 0, 600, 500),0,3)
-            window.blit(DEAD_MESSAGE_TEXT,(80, 60))
+            pygame.draw.rect(window, (255, 120, 219),pygame.Rect(0, 0, 600, 500), 0, 3)
+            window.blit(DEAD_MESSAGE_TEXT, (80, 60))
             if resume_b_d.draw(window):
                 Game(lvl)
             if quit_b_d.draw(window):
                 main_menu()
 
         pygame.display.update()
-    
+
     pygame.quit()
     exit()
+
 
 main_menu()
 pygame.quit()
--- player.py
+++ player.py
@@ -11,41 +11,42 @@ class Player(pygame.sprite.Sprite):
         self.flip = False
         self.window_height = WINDOW_SIZE[1]
         self.ATTACK_RECT_W = 14
-        self.ATTACK_RECT_W_WHEN_FLIPPED = -11 # when the frame is flipped the attack rect appears bigger if I only change the sign of the ATTACK_RECT_W .
-                                              #In consequence I have to make it bit shorter when flipped.
+        # when the frame is flipped the attack rect appears bigger if I only change the sign of the ATTACK_RECT_W .
+        self.ATTACK_RECT_W_WHEN_FLIPPED = -11
+        # In consequence I have to make it bit shorter when flipped.
 
-        self.hit_counter = 0 #get the number of times that the attack collider collided with the enemy collider during the attack animation
-        #animation related stuff
+        self.hit_counter = 0  # get the number of times that the attack collider collided with the enemy collider during the attack animation
+        # animation related stuff
         self.animation_list = []
         self.index = 0
         self.action = 0
         self.attack = False
         self.hit_enemy = False
         self.update_time = pygame.time.get_ticks()
-        #load idle anim frames/action 0
-        animation_types = ['idle', 'run', 'jump','attack']
+        # load idle anim frames/action 0
+        animation_types = ['idle', 'run', 'jump', 'attack']
         for animation in animation_types:
-        # reset temporary list of images
+            # reset temporary list of images
             temp_list = []
             # count number of files in the folder
             num_of_frames = len(os.listdir(f'assets/img/{animation}'))
             for i in range(num_of_frames):
-                p_img = pygame.image.load(os.path.join("assets","img",animation, f'{i}.png'))
+                p_img = pygame.image.load(os.path.join(
+                    "assets", "img", animation, f'{i}.png'))
                 temp_list.append(p_img)
             self.animation_list.append(temp_list)
 
         self.image = self.animation_list[self.action][self.index]
-        #self.rect = self.image.get_rect(topleft = pos)
-        #self.rect = self.rect.inflate(-11,0)
-        self.rect = pygame.Rect(pos,(6,16))
-        self.width =self.image.get_width()
+        # self.rect = self.image.get_rect(topleft = pos)
+        # self.rect = self.rect.inflate(-11,0)
+        self.rect = pygame.Rect(pos, (6, 16))
+        self.width = self.image.get_width()
         self.height = self.image.get_height()
-        
-        
 
-       #Player movement attributes
+       # Player movement attributes
         self.jump_intensity = -6
-        self.direction = pygame.math.Vector2(0,0)
+        self.is_jumping = False
+        self.direction = pygame.math.Vector2(0, 0)
         self.speed = 2
         self.moving_right = False
         self.moving_left = False
@@ -53,177 +54,165 @@ class Player(pygame.sprite.Sprite):
         self.air_timer = 0
         self.on_ground = True
 
-        #debug atributes
+        # debug atributes
         self.t_rect = False
-    
-    def attacks(self,target):
+
+    def attacks(self, target):
         self.hit = False
         if self.flip:
-            attack_rect = pygame.Rect(self.rect.centerx,self.rect.y, self.ATTACK_RECT_W_WHEN_FLIPPED ,self.rect.height)
+            attack_rect = pygame.Rect(self.rect.centerx, self.rect.y, self.ATTACK_RECT_W_WHEN_FLIPPED, self.rect.height)
         else:
-            attack_rect = pygame.Rect(self.rect.centerx,self.rect.y, self.ATTACK_RECT_W,self.rect.height)
+            attack_rect = pygame.Rect(self.rect.centerx, self.rect.y, self.ATTACK_RECT_W, self.rect.height)
         if attack_rect.colliderect(target):
-            if self.action == 3:#so that it doenst happen while other animations are running
+            if self.action == 3:  # so that it doenst happen while other animations are running
                 if self.hit_counter > 0:
                     pass
                 else:
                     self.hit_counter += 1
                     print("hit")
                     self.hit_enemy = True
-    
 
-
-    def movementANDcollisions(self,tile_rects,enemy_rect):
-        #Reset movement variables
+    def movementANDcollisions(self, tile_rects, enemy_rect):
+        # Reset movement variables
         dx = 0
         dy = 0
-        #gets inputs and the values that represent how much the player is gonna move
+        # gets inputs and the values that represent how much the player is gonna move
         if self.moving_left:
             self.direction.x = -1
-            dx = self.direction.x *self.speed
+            dx = self.direction.x * self.speed
             self.flip = True
-        if self.moving_right :
+        if self.moving_right:
             self.direction.x = 1
             dx = self.direction.x * self.speed
             self.flip = False
-         #attack inputs
+         # attack inputs
         if self.attack:
             self.attacks(enemy_rect)
-            #self.attack = False
-        #Apply gravity
+            # self.attack = False
+        # Apply gravity
+        
         self.direction.y += self.gravity
         if self.direction.y > 5:
             self.direction.y = 5
-        dy += self.direction.y    
-        
+
+        dy += self.direction.y
+
         self.width = self.rect.width
         self.height = self.rect.height
-        #Check for collisions
-        test = True
+        # Check for collisions
+        if self.direction.y > 0.9:
+            self.on_ground = False
         for tile in tile_rects:
-            #Check collision in the x direction
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y,self.width, self.height):
+            # Check collision in the x direction
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                 dx = 0
-            #Check collision in the y direction
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy,self.width, self.height):
-                #check if below ground i.e. jumping(ceiling collision)
+            # Check collision in the y direction
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check if below ground i.e. jumping(ceiling collision)
                 if self.direction.y < 0:
                     self.direction.y = 0
                     dy = tile[1].bottom - self.rect.top
-                    
 
-                #check if above the ground i.e falling (ground collision)
+                # check if above the ground i.e falling (ground collision)
                 elif self.direction.y >= 0:
-                   self.direction.y = 0
-                   dy = tile[1].top - self.rect.bottom
-                   #self.on_ground = True
-                   
-                
-            
-        #self.on_ground = not test
-        #if self.on_ground:
-        #    self.air_timer = 0
-        #else:
-        #    self.air_timer += 1
-       
-        #update player postion
-        self.rect.x += dx
-        self.rect.y += dy 
+                    self.direction.y += -self.direction.y
+                    dy = tile[1].top - self.rect.bottom
+                    self.on_ground = True
+                    #print(self.direction.y)
 
+        if self.on_ground:
+            self.air_timer = 0
+        else:
+            self.air_timer += 1
+
+        # update player postion
+        self.rect.x += dx
+        self.rect.y += dy
 
         if self.rect.centerx > 210 and self.direction.x > 0:
-            self.rect.x -= dx 
+            self.rect.x -= dx
             screen_scroll = -dx
         elif self.rect.centerx < 50 and self.direction.x < 0:
             self.rect.x -= dx
             screen_scroll = -dx
         else:
-            screen_scroll = 0 
-        return screen_scroll     
-        #if self.collision_types['bottom']:
-           #self.air_timer = 0
-        #else:
-            #self.air_timer += 1
+            screen_scroll = 0
+        return screen_scroll
 
     def jump(self):
-            self.direction.y = self.jump_intensity
-             
+        self.direction.y = self.jump_intensity
+        self.on_ground = False
+        self.air_timer = 0 
+
     def update(self):
         self.update_anim()
-        self.check_dead() 
-        
+        self.check_dead()
+
     def update_anim(self):
 
-        #update animation
+        # update animation
         ANIMATION_COOLDOWN = 150
-        #update img depending on current frame
+        # update img depending on current frame
         self.image = self.animation_list[self.action][self.index]
-        #if self.action == 1:
-            #self.attack = False
-        #check if enough time has passed since the last update
+        # if self.action == 1:
+        # self.attack = False
+        # check if enough time has passed since the last update
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.index += 1
-            #if the animation has run out , reset back to the start
+            # if the animation has run out , reset back to the start
             if self.index >= len(self.animation_list[self.action]):
                 if self.action == 3:
                     self.attack = False
                     self.hit_counter = 0
-                #if self.action == 1:
-                    #self.attack = False
+                # if self.action == 1:
+                    # self.attack = False
                 self.index = 0
-        #changes actions
+        # changes actions
         if not self.dead:
             if self.direction.y < 0:
-                self.update_action(2)#2:Jump
+                self.update_action(2)  # 2:Jump
 
             elif self.moving_right or self.moving_left:
-                    self.update_action(1)#1:run
+                self.update_action(1)  # 1:run
             elif self.attack:
-                self.update_action(3)#3:attack
+                self.update_action(3)  # 3:attack
             else:
-                    self.update_action(0)#0:idle
+                self.update_action(0)  # 0:idle
 
     def update_action(self, new_action):
-        
-        #check if new action is different to the previous
+
+        # check if new action is different to the previous
         if new_action != self.action:
             self.action = new_action
-            #update the anim settings
+            # update the anim settings
             self.index = 0
             self.update_time = pygame.time.get_ticks()
-            
-    
+
     def check_dead(self):
         if self.rect.y > self.window_height:
             self.dead = True
         else:
-            self.dead = False 
-    
+            self.dead = False
+
     def find_player_surface_blit_coordinates(self):
         image_width = self.image.get_width()
         middle_of_player_rect = self.rect.width/2
         middle_of_the_the_surface = image_width/2
         distance_between_middles = middle_of_the_the_surface - middle_of_player_rect
-        topleft_coordinates = (self.rect.x - distance_between_middles, self.rect.y)
+        topleft_coordinates = (
+            self.rect.x - distance_between_middles, self.rect.y)
         return topleft_coordinates
-          
-    def draw(self,display):
-        player_render_coordinates = self.find_player_surface_blit_coordinates()
-        #if self.flip and self.action == 1:
-            #display.blit(pygame.transform.flip(self.image,self.flip,False), (self.rect.x - 7 , self.rect.y))
-        
-        #elif self.flip and self.action == 2:
-            #display.blit(pygame.transform.flip(self.image,self.flip,False), (self.rect.x - 7 , self.rect.y))
 
-        #elif self.flip and self.action == 3:
-            #display.blit(pygame.transform.flip(self.image,self.flip,False), (self.rect.x - 7 , self.rect.y))
-        
-        #else:
-        display.blit(pygame.transform.flip(self.image,self.flip,False), player_render_coordinates)
+    def draw(self, display):
+        player_render_coordinates = self.find_player_surface_blit_coordinates()
+
+        display.blit(pygame.transform.flip(self.image, self.flip, False), player_render_coordinates)
         if self.t_rect:
-            pygame.draw.rect(display, (255,0,0), self.rect,  1)
+            pygame.draw.rect(display, (255, 0, 0), self.rect,  1)
             if self.flip:
-                pygame.draw.rect(display, (0,255,0), pygame.Rect(self.rect.centerx,self.rect.y, self.ATTACK_RECT_W_WHEN_FLIPPED ,self.rect.height) ,  1)
-            else :
-                pygame.draw.rect(display, (0,255,0), pygame.Rect(self.rect.centerx,self.rect.y, self.ATTACK_RECT_W,self.rect.height) ,  1)
+                pygame.draw.rect(display, (0, 255, 0), pygame.Rect(
+                    self.rect.centerx, self.rect.y, self.ATTACK_RECT_W_WHEN_FLIPPED, self.rect.height),  1)
+            else:
+                pygame.draw.rect(display, (0, 255, 0), pygame.Rect(
+                    self.rect.centerx, self.rect.y, self.ATTACK_RECT_W, self.rect.height),  1)
